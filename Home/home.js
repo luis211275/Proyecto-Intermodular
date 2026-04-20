d24e198740b4cf5bbf580e854fc2222d32d84561
danieloo72 <daniel07nieto@gmail.com>
2026-04-20 14:54:58 +0200

    1v

---
+++ Home/home.js
@@ -0,0 +1,21 @@
+const buscarCoches = document.getElementById("buscar");
+const btnReset = document.getElementById("resetear");
+const btnFav = document.getElementById("favoritos");
+
+const modalCoche = document.getElementById("modalCoche");
+const modalTitulo = document.getElementById("modalTitulo");
+const btnCerrarModal = document.getElementById("cerrarModal");
+const modalImg = document.getElementById("modalImagen");
+const modalAnio = document.getElementById("modalAnio");
+const modalPrecio = document.getElementById("modalPrecio");
+const modalColor = document.getElementById("modalColor");
+const modalCarburante = document.getElementById("modalCarburante");
+const modalTransmision = document.getElementById("modalTransmision");
+const modalKM = document.getElementById("modalKM");
+const modalCiudad = document.getElementById("modalCiudad");
+const modalDescripcion = document.getElementById("modalDescripcion");
+
+function abrirModal(coche) {
+    modalTitulo.innerText = `${coche.marca} ${coche.modelo}`;
+    
+}
