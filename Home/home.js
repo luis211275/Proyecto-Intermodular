1f28ea0928dc9a7663544661b01225da4f065180
danieloo72 <daniel07nieto@gmail.com>
2026-04-21 13:12:22 +0200

    1v

--- Home/home.js
+++ Home/home.js
@@ -2,7 +2,7 @@ const buscarCoches = document.getElementById("buscar");
 const btnReset = document.getElementById("resetear");
 const btnFav = document.getElementById("favoritos");
 
-const modalCoche = document.getElementById("modalCoche");
+const modal = document.getElementById("modalCoche");
 const modalTitulo = document.getElementById("modalTitulo");
 const btnCerrarModal = document.getElementById("cerrarModal");
 const modalImg = document.getElementById("modalImagen");
@@ -16,6 +16,19 @@ const modalCiudad = document.getElementById("modalCiudad");
 const modalDescripcion = document.getElementById("modalDescripcion");
 
 function abrirModal(coche) {
-    modalTitulo.innerText = `${coche.marca} ${coche.modelo}`;
-    
+    modalTitulo.innerText = `${coche}`;
+    modalAnio
+    modalPrecio
+    modalColor
+    modalCarburante
+    modalTransmision
+    modalKM
+    modalCiudad
+    modalDescripcion
+
+    modal.showModal();
 }
+
+btnCerrarModal.addEventListener("click", () => {
+    modal.close();
+});
