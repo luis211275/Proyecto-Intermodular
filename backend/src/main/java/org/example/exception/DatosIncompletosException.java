56ad166ebb93950abdaaf81dcbd0f8ab9302cddf
Julio Rodríguez <juliorodriguez@vdenergy.es>
2026-05-15 18:13:00 +0200

    test terminados, insert dml

--- backend/src/main/java/org/example/exception/DatosIncompletosException.java
+++ backend/src/main/java/org/example/exception/DatosIncompletosException.java
@@ -1,14 +1,8 @@
 package org.example.exception;
 
-/**
- * Excepción para errores de datos incompletos.
- */
+
 public class DatosIncompletosException extends Exception {
-  /**
-   * Instancia una nueva excepción de datos incompletos.
-   *
-   * @param message el mensaje de error.
-   */
+
   public DatosIncompletosException(String message) {
     super(message);
   }
--- backend/src/main/java/org/example/exception/ErrorDeAccesoADatosException.java
+++ backend/src/main/java/org/example/exception/ErrorDeAccesoADatosException.java
@@ -1,15 +1,7 @@
 package org.example.exception;
 
-/**
- * Excepción para errores de acceso a datos.
- */
 public class ErrorDeAccesoADatosException extends Exception {
 
-  /**
-   * Instancia una nueva excepción de error de acceso a datos.
-   *
-   * @param message el mensaje de error.
-   */
   public ErrorDeAccesoADatosException(String message) {
     super(message);
   }
